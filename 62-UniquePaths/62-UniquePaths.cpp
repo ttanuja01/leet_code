05575973db5881f9212b324d94c06c5392bb3341
Sai Tanuja Thirupathi <164322103+ttanuja01@users.noreply.github.com>
2025-05-18 08:40:56 +0530

    File updated at 5/18/2025, 8:40:55 AM

--- 62-UniquePaths/62-UniquePaths.cpp
+++ 62-UniquePaths/62-UniquePaths.cpp
@@ -1,19 +1,27 @@
-// Last updated: 5/18/2025, 8:33:53 AM
+// Last updated: 5/18/2025, 8:40:55 AM
 class Solution {
 public:
-    int findAllUniquePaths(int i,int j,int m,int n,int &c, vector<vector<int>>&v){
-        if(i<0 || j<0)return 0;
-        if(v[i][j]!=0)return v[i][j];
-        if(i==0 && j==0){
-            return v[i][j]=1;
+    int findAllUniquePaths(int i, int j, int m, int n, int& c,
+                           vector<vector<int>>& v) {
+        if (i < 0 || j < 0)
+            return 0;
+        if (v[i][j] != 0)
+            return v[i][j];
+        if (i == 0 && j == 0) {
+            return v[i][j] = 1;
         }
-        return v[i][j]= findAllUniquePaths(i-1,j,m,n,c,v)+ findAllUniquePaths(i,j-1,m,n,c,v);
+        return v[i][j] = findAllUniquePaths(i - 1, j, m, n, c, v) +
+                         findAllUniquePaths(i, j - 1, m, n, c, v);
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>v(m,vector<int>(n,0));
-        int count=0;
-       return findAllUniquePaths(m-1,n-1,m,n,count,v);
-        // return count;
+        vector<vector<int>> v(m, vector<int>(n, 1));
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                v[i][j]=v[i-1][j]+v[i][j-1];
+
+            }
+        }
+        return v[m-1][n-1];
     }
 };
 // 1 0
