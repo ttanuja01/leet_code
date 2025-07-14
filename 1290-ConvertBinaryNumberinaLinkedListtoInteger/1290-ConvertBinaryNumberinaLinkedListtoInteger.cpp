f58ea9bd2e5730c9e2ad955a51f4c32de6b40765
Sai Tanuja Thirupathi <164322103+ttanuja01@users.noreply.github.com>
2025-07-14 22:32:54 +0530

    Create 1290-ConvertBinaryNumberinaLinkedListtoInteger

---
+++ 1290-ConvertBinaryNumberinaLinkedListtoInteger/1290-ConvertBinaryNumberinaLinkedListtoInteger.cpp
@@ -0,0 +1,22 @@
+// Last updated: 7/14/2025, 10:32:54 PM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        int result = 0;
+        while (head != nullptr) {
+            result = (result << 1) | head->val;
+            head = head->next;
+        }
+        return result;
+    }
+};
